e8dd335e71de380c604964618413b9a6850a4e18
Brian Mulindi <mulindijrn@gmail.com>
2023-09-14 18:44:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+}
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "the birthday party";
+const thankYouMessages = writeCards(namesArray, eventName);
+
+console.log(thankYouMessages);
+
+
+function countDown(number){
+    let i = number;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+    return number;
+}
+countDown(10);
+  
+
+  
